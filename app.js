aa49e58e1351f09b0dd8c7030949bee9895bfbc2
rlasse <rau.lasse@gmail.com>
2022-08-13 15:05:53 -0400

    Just a test

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /* global config csv2geojson turf Assembly $ */
 'use strict';
+// test
+
 
 mapboxgl.accessToken = "pk.eyJ1IjoidGhlamFtZXNoZWFyZCIsImEiOiJjbDZndjluN2YwMDF3M2JyeG52dHFoaXV1In0.clP391payV0qfNDtemgiag";
 const columnHeaders = config.sideBarInfo;

bea12abcbee19a1d7691f3406149212c6b9a911a
rlasse <67561744+rlasse@users.noreply.github.com>
2022-08-16 14:16:14 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// eslint-disable-next-line no-unused-vars
-const config = {
-  title: 'Replace with your title',
-  description:
-    'Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.',
-};
